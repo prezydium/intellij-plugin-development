c9c173384d97a18d0c45b108b5a0aeefff285b49
Krzysztof Syska <prezydium834@gmail.com>
2018-06-30 10:18:09 +0200

    Initial Commit

---
+++ src/org/prezydium/DisplayProjectProperties.java
@@ -0,0 +1,12 @@
+package org.prezydium;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class DisplayProjectProperties extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        System.out.println("hello");
+    }
+}
